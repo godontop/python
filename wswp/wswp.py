c645710111ecfba575c342d2adb2f827780f6977
godontop <tosony@126.com>
2018-05-06 22:22:40 +0800

    add robots.txt parse function

--- wswp/wswp.py
+++ wswp/wswp.py
@@ -5,7 +5,7 @@ import urllib.error
 import urllib.parse
 
 
-def download(url, user_agent='wswp', num_retries=2):
+def download(url, user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36', num_retries=2):
     print('Downloading:', url)
     headers = {'User-agent': user_agent}
     request = urllib.request.Request(url, headers=headers)
@@ -25,14 +25,21 @@ def link_crawler(seed_url, link_regex):
     """Crawl from the given seed URL following links matched by link_regex
     """
     crawl_queue = [seed_url]
+    # keep track which URL's have seen before
+    seen = set(crawl_queue)
     while crawl_queue:
         url = crawl_queue.pop()
         html = download(url)
         # filter for links matching our regular expression
         for link in get_links(html):
+            # check if link matches expected regex
             if re.match(link_regex, link):
+                # form absolute link
                 link = urllib.parse.urljoin(seed_url, link)
-                crawl_queue.append(link)
+                # check if have already seen this link
+                if link not in seen:
+                    seen.add(link)
+                    crawl_queue.append(link)
 
 
 def get_links(html):
