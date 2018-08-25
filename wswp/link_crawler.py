dc6f42d85bf0fe1f9812c5764ab4f56e31d8eb37
godontop <tosony@126.com>
2018-08-25 16:07:48 +0800

    delete download function and Throttle class from link_crawler.py

--- wswp/link_crawler.py
+++ wswp/link_crawler.py
@@ -10,62 +10,6 @@ import sys
 from downloader import Downloader
 
 
-user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'
-rp = urllib.robotparser.RobotFileParser()
-rp.set_url('http://example.webscraping.com/robots.txt')
-rp.read()
-proxy = None
-
-
-class Throttle:
-    """Add a delay between downloads to the same domain
-    """
-
-    def __init__(self, delay):
-        # amount of delay between downloads for each domain
-        self.delay = delay
-        # timestamp of when a domain was last accessed
-        self.domains = {}
-
-    def wait(self, url):
-        domain = urllib.parse.urlparse(url).netloc
-        last_accessed = self.domains.get(domain)
-        if self.delay > 0 and last_accessed is not None:
-            sleep_secs = self.delay - \
-                (datetime.datetime.now() - last_accessed).seconds
-            if sleep_secs > 0:
-                # domain has been accessed recently
-                # so need to sleep
-                time.sleep(sleep_secs)
-        # update the last accessed time
-        self.domains[domain] = datetime.datetime.now()
-
-
-throttle = Throttle(1)
-
-
-def download(url, user_agent='wswp', proxy=None, num_retries=2):
-    '''Support custom User-Agent, proxy and auto retry
-    '''
-    print('Downloading:', url)  # url is download function's first arguments
-    headers = {'User-agent': user_agent}  # user_agent is the second arguments
-    request = urllib.request.Request(url, headers=headers)
-    opener = urllib.request.build_opener()
-    if proxy:
-        proxy_params = {urllib.parse.urlparse(url).scheme: proxy}
-        opener.add_handler(urllib.request.ProxyHandler(proxy_params))
-    try:
-        html = opener.open(request).read().decode()
-    except urllib.error.URLError as e:
-        print('Download error:', e.reason)
-        html = None
-        if num_retries > 0:
-            if hasattr(e, 'code') and 500 <= e.code < 600:
-                # retry 5XX HTTP errors
-                html = download(url, user_agent, proxy, num_retries - 1)
-    return html
-
-
 def link_crawler(seed_url, link_regex, delay=1, max_depth=2, proxies=None, num_retries=2, scrape_callback=None, cache=None):
     """Crawl from the given seed URL following links matched by link_regex
     """
