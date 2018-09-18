a40133ec7f674bd1e8ca47699ee796a599e0661c
godontop <tosony@126.com>
2018-09-18 17:38:12 +0800

    test file

--- tmp.py
+++ tmp.py
@@ -1,11 +1,16 @@
-from urllib.request import urlopen
+# coding: utf8
+from urllib.request import build_opener, Request, ProxyHandler
+from urllib.parse import urlparse
 
 
-def download_simple(url):  # url(str)
-    """The simplest download function
-    """
-    html = urlopen(url).read().decode()
-    return html
-
-
-print(download_simple('http://example.webscraping.com').strip())
+url = 'https://youtube.com'
+User_Agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.183 Safari/537.36 Vivaldi/1.96.1147.64'
+headers = {'User-Agent': User_Agent}
+req = Request(url, headers=headers)
+protocol = urlparse(url).scheme
+server = '127.0.0.1:1080'
+proxies = {protocol: server}
+opener = build_opener()
+opener.add_handler(ProxyHandler(proxies))
+page = opener.open(req).read().decode()
+print(page)
