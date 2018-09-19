28d46e138a24e0acb8225e276a18334cc6855b0a
godontop <tosony@126.com>
2018-09-19 15:14:09 +0800

    rename link_crawler to crawler

--- wswp/sequential_test.py
+++ wswp/sequential_test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from link_crawler import link_crawler
+from crawler import crawler
 from mongo_cache import MongoCache
 from alexa_callback import AlexaCallback
 
@@ -9,7 +9,7 @@ def main():
 	scrape_callback = AlexaCallback()
 	cache = MongoCache()
 	# cache.clear()
-	link_crawler(scrape_callback.seed_url, proxies=['127.0.0.1:8118',], scrape_callback=scrape_callback, cache=cache)
+	crawler(scrape_callback.seed_url, proxies=['127.0.0.1:8118',], scrape_callback=scrape_callback, cache=cache)
 
 
 if __name__ == '__main__':
