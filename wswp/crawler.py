5bd591fab300e335668133909afe978d5fb8aeb7
godontop <tosony@126.com>
2018-09-19 17:49:25 +0800

    discard robots.txt parse function

--- wswp/crawler.py
+++ wswp/crawler.py
@@ -14,30 +14,10 @@ def crawler(seed_url, delay=1, max_depth=2, user_agent='Mozilla/5.0 (Macintosh; 
     """Crawl from the given seed URL following links matched by link_regex
     """
     crawl_queue = [seed_url]
-    # keep track which URL's have seen before
-    seen = {seed_url: 0}
-    num_urls = 0
-    rp = get_robots(seed_url)
     D = Downloader(delay=delay, user_agent=user_agent, proxies=proxies,
                    num_retries=num_retries, cache=cache)
     if scrape_callback:
         crawl_queue = scrape_callback('http://s3.amazonaws.com/alexa-static/top-1m.csv.zip', D('http://s3.amazonaws.com/alexa-static/top-1m.csv.zip'))
     while crawl_queue:
         url = crawl_queue.pop()
-        # check url passes robots.txt restrictions
-        if rp.can_fetch(user_agent, url):
-            html = D(url)
-        else:
-            print('Blocked by robots.txt:', url)
-            print(user_agent)
-            html = None
-            sys.exit()
-
-
-def get_robots(url):
-    """Initialize robots parser for this domain
-    """
-    rp = urllib.robotparser.RobotFileParser()
-    rp.set_url(urllib.parse.urljoin(url, '/robots.txt'))
-    rp.read()
-    return rp
+        html = D(url)
