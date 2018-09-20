44a875a30caa85340739235989976c3f0875d684
godontop <tosony@126.com>
2018-09-20 21:28:50 +0800

    update bad urls

--- wswp/alexa_callback.py
+++ wswp/alexa_callback.py
@@ -35,7 +35,8 @@ class AlexaCallback:
                         'bestbuy.com', 'sjtu.edu.cn', 'retailmenot.com', 'amazon.cn',
                         'hulu.com', 'thepiratebay.org', 'sozcu.com.tr', 'exdynsrv.com',
                         'myshopify.com', 'twimg.com', 'youporn.com', 'ebc.net.tw',
-                        'artnewsupdate.info']
+                        'artnewsupdate.info', 'cloudfront.net', 'dailymotion.com',
+                        '9amq5z4y1y.com']
             with ZipFile(BytesIO(html)) as zf:
                 csv_filename = zf.namelist()[0]
                 for rank, website in csv.reader(StringIO(zf.open(csv_filename).read().decode())):
