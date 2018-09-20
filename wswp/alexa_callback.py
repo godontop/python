f9c9f600ab4be9972d9dc8e3026823e1e5a61858
godontop <tosony@126.com>
2018-09-20 17:26:26 +0800

    update bad urls

--- wswp/alexa_callback.py
+++ wswp/alexa_callback.py
@@ -27,7 +27,8 @@ class AlexaCallback:
                         'cbssports.com', 'adp.com', 'torrent9.ph', 'wixsite.com',
                         'slots777.shop', 'mercantilbanco.com', 'alicdn.com', '104.com.tw',
                         'digitaldsp.com', 'namu.wiki', 'subscene.com', 'exhentai.org',
-                        'myanimevideo.club', 'accuweather.com']
+                        'myanimevideo.club', 'accuweather.com', 'kissanime.ru',
+                        'jf71qh5v14.com']
             with ZipFile(BytesIO(html)) as zf:
                 csv_filename = zf.namelist()[0]
                 for rank, website in csv.reader(StringIO(zf.open(csv_filename).read().decode())):
