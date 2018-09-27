5da67a98e2b5ef064a5a5fe5540d5d38712b9407
godontop <tosony@126.com>
2018-09-27 21:53:39 +0800

    update bad urls

--- wswp/alexa_callback.py
+++ wswp/alexa_callback.py
@@ -44,7 +44,7 @@ class AlexaCallback:
                         'shopee.tw', 'thewhizproducts.com', 'tim.it', 'tomshardware.com',
                         'iherb.com', 'nicenewsupdate.info', 'epfindia.gov.in',
                         'cnzz.com', 'v2ex.com', 'td.com', 'prothomalo.com', 'free.fr',
-                        'neobux.com', 'jma.go.jp', 'lee.tmall.com']
+                        'neobux.com', 'jma.go.jp', 'lee.tmall.com', 'bamilo.com']
             with ZipFile(BytesIO(html)) as zf:
                 csv_filename = zf.namelist()[0]
                 for rank, website in csv.reader(StringIO(zf.open(csv_filename).read().decode())):
