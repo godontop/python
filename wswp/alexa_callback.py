7572d721d1a329fa0e387aeac05ee482f7ec7b42
godontop <tosony@126.com>
2018-09-27 14:54:22 +0800

    update bad urls

--- wswp/alexa_callback.py
+++ wswp/alexa_callback.py
@@ -38,7 +38,7 @@ class AlexaCallback:
                         'artnewsupdate.info', 'cloudfront.net', 'dailymotion.com',
                         '9amq5z4y1y.com', 'exosrv.com', 'tianya.cn', 'detail.tmall.com',
                         'googleusercontent.com', 'microsoftonline.com', 'pornhub.com',
-                        'zomato.com', 'ipleer.fm']
+                        'zomato.com', 'ipleer.fm', 'cbc.ca']
             with ZipFile(BytesIO(html)) as zf:
                 csv_filename = zf.namelist()[0]
                 for rank, website in csv.reader(StringIO(zf.open(csv_filename).read().decode())):
