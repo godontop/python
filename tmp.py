5439f4d489b1232f022fe8c26c238384f1640485
godontop <tosony@126.com>
2018-10-05 23:55:13 +0800

    add threading download alexa sites

--- tmp.py
+++ tmp.py
@@ -3,7 +3,7 @@ from urllib.request import build_opener, Request, ProxyHandler
 from urllib.parse import urlparse
 
 
-url = 'https://youtube.com'
+url = 'http://'
 User_Agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.183 Safari/537.36 Vivaldi/1.96.1147.64'
 headers = {'User-Agent': User_Agent}
 req = Request(url, headers=headers)
