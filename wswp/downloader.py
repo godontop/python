8fabee45f351ee271693cb4723430a2390dbca52
godontop <tosony@126.com>
2018-10-06 17:04:24 +0800

    add a timeout argument for Downloader class's dunder init

--- wswp/downloader.py
+++ wswp/downloader.py
@@ -6,10 +6,13 @@ import time
 
 
 DEFAULT_AGENT = 'wswp'
+DEFAULT_TIMEOUT = 60
+
 
 class Downloader:
 
-    def __init__(self, delay=1, user_agent=DEFAULT_AGENT, proxies=None, num_retries=1, cache=None):
+    def __init__(self, delay=1, user_agent=DEFAULT_AGENT, proxies=None,
+                 num_retries=1, timeout=DEFAULT_TIMEOUT, cache=None):
         self.throttle = Throttle(delay)
         self.user_agent = user_agent
         self.proxies = proxies
